d53f3bef1a0f4ce58a539437f30eb6ed4b47c57e
emmaMarful <eamarful@gmail.com>
2023-02-25 02:23:19 +0000

    added a match case event

--- convertor/gui.py
+++ convertor/gui.py
@@ -7,9 +7,8 @@ feetInput = user_interface.InputText(key="feetKey")
 entInches = user_interface.Text("Enter inches:")
 incheInput = user_interface.InputText(key="inchKey")
 
-convertBtn = user_interface.Button("Convert", key="convertKey")
+convertBtn = user_interface.Button("Convert")
 output = user_interface.Text (key="answer")
-
 
 layout = [[entFeet, feetInput], [entInches, incheInput], [convertBtn, output]]
 windows = user_interface.Window("Convertor", layout, font=("Helvetica",16))
@@ -18,13 +17,17 @@ while True:
     event, values = windows.read()
     print(event)
     print(values)
+    match event:
+        case "Convert":
+            feet = float(values["feetKey"])
+            inches = float (values["inchKey"])
 
+            results = calculateToMeter(feet,inches)
+            print(results)
+            windows['answer'].update(value=f"{results}m", text_color="white")
 
-    feetInput = float(values["feetKey"])
-    incheInput = float(values["inchKey"])
-
-    results = calculateToMeter(feetInput, incheInput)
-    windows["answer"].update(value=f"{results} m", text_color="white")
+        case user_interface.WIN_CLOSED:
+            break
 
 windows.close()
 
